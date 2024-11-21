02c84f08fb9eb916ec8b0e126bf86323a1f68126
danny <dannytakushi@gmail.com>
2024-11-20 18:27:33 -0600

    een app factored into different section

---
+++ app/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include "een.h"
+
+void exit_application(int signum) {
+  std::cout  << "exiting sub application..."<<std::endl;
+  exit(0);
+}
+void sig_int_handler(int signum) {
+  std::cout << std::endl;
+  std::cout << "ctrl+c pressed, exiting..."<<std::endl;
+  exit_application(1);
+}
+
+static std::string time_rfc_3339() {
+  /** https://en.cppreference.com/w/cpp/chrono/c/strftim */
+  std::time_t time = std::time({});
+  char timeString[std::size("yyyy-mm-ddThh:mm:ssZ")];
+  std::strftime(std::data(timeString), std::size(timeString),
+                "%FT%TZ", std::gmtime(&time));
+  return timeString;
+}
+
+int main(int argc, char* argv[]) {
+  /** print app version determined from root CMakeLists.txt */
+  std::cout  << time_rfc_3339() <<" : ";
+  std::cout << std::string(APP_VERSION) << " starting..." <<  std::endl;
+  std::shared_ptr<een> een_ = std::make_shared<een>("");
+
+  while(een_->is_stable()){
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+  std::cout << "exiting main.  goodbye."<<std::endl;
+}
