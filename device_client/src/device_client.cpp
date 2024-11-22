77abfc46d254c2ff1b39c55fc6408551ffcbb6c1
danny <dannytakushi@gmail.com>
2024-11-22 10:24:43 -0600

    device_client::get_ddata() add attributes to payload

--- device_client/src/device_client.cpp
+++ device_client/src/device_client.cpp
@@ -31,7 +31,38 @@ org_eclipse_tahu_protobuf_Payload* device_client::get_ddata(){
   org_eclipse_tahu_protobuf_Payload* ddata_payload = new org_eclipse_tahu_protobuf_Payload;
   get_next_payload(ddata_payload);
 
-  attribute_host_.ddata_gen(ddata_payload);
+  for(auto attr : attribute_host_.get_updated_attributes()){
+    size_t attr_size;
+    uint64_t tahu_datatype;
+    switch (attr->get_datatype()){
+      case kInteger:
+        tahu_datatype = METRIC_DATA_TYPE_INT64;
+        attr_size = sizeof(uint64_t);
+        break;
+      case kDouble:
+        tahu_datatype = METRIC_DATA_TYPE_DOUBLE;
+        attr_size = sizeof(double);
+        break;
+      case kString:
+        tahu_datatype = METRIC_DATA_TYPE_STRING;
+        attr_size = sizeof(std::string);
+        break;
+      default:
+        std::cout << "unknown attribute type!" << std::endl;
+        break;
+    }
+
+    add_simple_metric(ddata_payload,
+                      attr->get_name().c_str(),
+                      true,
+                      0, // alias; should maybe be removed
+                      tahu_datatype,
+                      false,
+                      false,
+                      attr->get_value(),
+                      attr_size);
+  }
+
   if(ddata_payload->metrics_count <= 0){
     delete ddata_payload;
     ddata_payload = NULL;
