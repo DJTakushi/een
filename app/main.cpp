ff6fe227ad28930e60789ce937efc58f0a850c92
danny <dannytakushi@gmail.com>
2024-12-05 11:56:49 -0600

    arg_helper renamed from argument_helper

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <thread>
 #include "een_factory.h"
-#include "argument_helper.h"
+#include "arg_helper.h"
 
 void exit_application(int signum) {
   std::cout  << "exiting sub application..."<<std::endl;
@@ -27,10 +27,10 @@ int main(int argc, char* argv[]) {
   std::cout  << time_rfc_3339() <<" : ";
   std::cout << std::string(EEN_VERSION) << " starting..." <<  std::endl;
 
-  std::string spb_address = ec::argument_helper::get_spb_address(argc,argv);
-  ec::connection_type type = ec::argument_helper::get_connection_type(argc,argv);
-  std::string local_address = ec::argument_helper::get_address(argc, argv);
-  uint local_port = ec::argument_helper::get_port(argc, argv);
+  std::string spb_address = ec::arg_helper::get_spb_address(argc,argv);
+  ec::connection_type type = ec::arg_helper::get_connection_type(argc,argv);
+  std::string local_address = ec::arg_helper::get_address(argc, argv);
+  uint local_port = ec::arg_helper::get_port(argc, argv);
 
   std::shared_ptr<een_i> een_ = een_factory::create_een("",
                                                         spb_address,
